ef5b311e9c6aab4c7ff5ee85fdc4e1eb6ce21a20
karyna4k <karyna.kananovich@gmail.com>
2020-03-24 12:09:16 +0300

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -80,4 +80,20 @@ $(document).ready(function() {
     valideForms('#order form');
 
     $('input[name=tel]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });

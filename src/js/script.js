8bc79f87b49f2041b3e7b09b9f106dc5df39c661
karyna4k <karyna.kananovich@gmail.com>
2020-03-25 12:19:06 +0300

    add file _animations.scss

--- src/js/script.js
+++ src/js/script.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
     });
 
     $("a[href^='#']").click(function() {
-        var _href = $(this).attr("href");
+        const _href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
     });
